1b2e1bebf9a5d0734ac71e1de0bee34c329eca7c
Muh Reza Fajar <muh.reza@cartenz.co.id>
2019-03-29 17:22:38 +0700

    First Commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const KuwuxColorSquare = props => {
+  const { width, height, color, text } = props;
+  return (
+    <div
+      style={{
+        width: width || 100,
+        height: height || 100,
+        backgroundColor: color || "blue"
+      }}
+    >
+      {text}
+    </div>
+  );
+};
+
+export default KuwuxColorSquare;
